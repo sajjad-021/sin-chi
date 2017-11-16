8e0509fbe6699c27057cc8230442221d83acc34c
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-11-15 16:54:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 cd $HOME/sin-chi
-wget https://valtman.name/files/telegram-bot-171022-nightly-linux
-mv telegram-bot-171022-nightly-linux tb
+wget https://valtman.name/files/telegram-bot-171115-nightly-linux
+mv telegram-bot-171115-nightly-linux tb
 
- sudo apt-get update -y 
- sudo apt-get upgrade -y
  sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 sudo apt-get install lua-lgi -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get install libstdc++6 -y
  sudo apt-get install libstdc++6 -y
- sudo apt-get update -y
- sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
  cd luarocks-2.2.2
